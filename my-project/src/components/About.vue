27719dce2be1252cb003b1782bdaf6f5bb0b513b
LEESYUE <690298275@qq.com>
2019-07-25 18:41:30 +0800

    First commit

---
+++ my-project/src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h>about</h>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
